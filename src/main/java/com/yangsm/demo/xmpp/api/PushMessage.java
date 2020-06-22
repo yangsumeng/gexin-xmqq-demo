2f5219ac3a01452a2fd9e2595936dc42ed19db82
yangsumeng <bmeB@500>
2020-06-22 20:53:56 +0800

    init

---
+++ src/main/java/com/yangsm/demo/xmpp/api/PushMessage.java
@@ -0,0 +1,16 @@
+package com.yangsm.demo.xmpp.api;
+
+/**
+ * @Author: yangsm
+ * @Date: 2020/06/22/20:10
+ * @Description:
+ */
+public class PushMessage {
+    private Integer model;
+    private String sevCId;
+    private String title;
+    private String content;
+    private String iconUrl;
+    private String msgId;
+    private String intent;
+}
